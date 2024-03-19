ac0cb236945cdb43ac9db673220c26a4c0a824ca
Kyle Lou <77580846+Kyle-Lou@users.noreply.github.com>
2024-03-18 22:43:44 -0400

    Add files via upload
    new app.js for dummy web

---
+++ backend/templates/app.py
@@ -0,0 +1,45 @@
+import json
+import os
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
+import pandas as pd
+
+# ROOT_PATH for linking with all your files. 
+# Feel free to use a config.py or settings.py with a global export variable
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+
+# Get the directory of the current script
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the path to the JSON file relative to the current script
+json_file_path = os.path.join(current_directory, 'test.json')
+
+# Assuming your JSON data is stored in a file named 'init.json'
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+app = Flask(__name__)
+CORS(app)
+
+# Sample search using json with pandas
+def json_search(query):
+    matches = []
+    for i in range(len(data)):
+       if query.lower() in data[i]['title'].lower():
+           matches.append(data[i]['title'])
+    print(matches)
+    return matches
+
+@app.route("/")
+def home():
+    return render_template('base.html',title="sample html")
+
+@app.route("/episodes")
+def episodes_search():
+    text = request.args.get("title")
+    return json_search(text)
+
+
+if 'DB_NAME' not in os.environ:
+    app.run(debug=True,host="0.0.0.0",port=5000)
